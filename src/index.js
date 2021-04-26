1592be28f0bf31c790bba6a6c7aa6a47096a98f1
OdongAlican <sandieo.2020@gmail.com>
2021-04-26 23:20:12 +0300

    update index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import './index.css';
 import rootReducer from './reducers';
 import Router from './components/Router';
 
