42c527ef906113b6a0cd1effd978480616f9e50c
OdongAlican <sandieo.2020@gmail.com>
2021-04-26 23:21:21 +0300

    update api.js file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,13 @@
-/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
 const baseUrl = 'https://api-test.innoloft.com';
 
 export const FetchProductRequest = async (method, path) => {
   const response = await axios[method](`${baseUrl}${path}`);
+  return response;
+};
+
+export const UpdateProductRequest = async (method, path, data) => {
+  const response = await axios[method](`${baseUrl}${path}`, data);
   return response;
 };
