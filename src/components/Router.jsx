5f046a6474aca5df5fb666a13e8f68256ad89492
OdongAlican <sandieo.2020@gmail.com>
2021-04-26 20:39:13 +0300

    add sidebar route

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
+import Sidebar from './Sidebar/Sidebar';
 
 const Router = () => (
   <div>
     <BrowserRouter>
       <Navbar />
+      <Sidebar />
     </BrowserRouter>
   </div>
 );
