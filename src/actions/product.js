61c526028eec7bcc7be75c34f14395949a1d7ace
OdongAlican <sandieo.2020@gmail.com>
2021-04-26 23:18:06 +0300

    update product action

--- src/actions/product.js
+++ src/actions/product.js
@@ -1,11 +1,17 @@
-import { FetchProductRequest } from '../utils/api';
+import { FetchProductRequest, UpdateProductRequest } from '../utils/api';
 
 export const FETCH_PRODUCT_SUCCESS = 'FETCH_PRODUCT_SUCCESS';
+export const FETCH_PRODUCT_FAILURE = 'FETCH_PRODUCT_FAILURE';
 export const FETCH_PRODUCT_LOADING = 'FETCH_PRODUCT_LOADING';
 
 export const fetchProductSuccess = product => ({
   type: FETCH_PRODUCT_SUCCESS,
   payload: product,
+});
+
+export const fetchProductFailure = error => ({
+  type: FETCH_PRODUCT_FAILURE,
+  payload: error,
 });
 
 export const fetchProduct = id => async dispatch => {
@@ -15,7 +21,21 @@ export const fetchProduct = id => async dispatch => {
     dispatch({ type: FETCH_PRODUCT_LOADING });
     const response = await FetchProductRequest(method, path);
     console.log(response.data);
+    dispatch(fetchProductSuccess(response.data));
   } catch (error) {
+    dispatch(fetchProductFailure(error.message));
+  }
+};
+
+export const updateProduct = data => async dispatch => {
+  const method = 'put';
+  const path = `/product/${data.id}/`;
+  console.log(data, 'detailed data values');
+  try {
+    const response = await UpdateProductRequest(method, path, data);
+    dispatch(fetchProductSuccess(response.data));
+  } catch (error) {
+    dispatch(fetchProductFailure(error.message));
     console.log(error.message);
   }
 };
