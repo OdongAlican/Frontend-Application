9f5ef9379e61ba710565e5f2bf1e71b62fa6e1f9
OdongAlican <sandieo.2020@gmail.com>
2021-04-26 23:18:41 +0300

    update routes

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Main from './Main/Main';
+import Product from './Product/Product';
 import Navbar from './Navbar/Navbar';
 import Sidebar from './Sidebar/Sidebar';
 
@@ -8,6 +10,12 @@ const Router = () => (
     <BrowserRouter>
       <Navbar />
       <Sidebar />
+      <div className="switch-section">
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/product/:id" component={Product} />
+        </Switch>
+      </div>
     </BrowserRouter>
   </div>
 );
